52e8f86737f3353452d59e4659f9c2a8c48bf9f7
kong0107 <kong0107@gmail.com>
2018-11-14 01:38:30 +0800

    add one optional parameter to `replaceText`; functionlize some codes in `replaceText` to `strSplitAndJoinByRules`

--- domCrawler.js
+++ domCrawler.js
@@ -6,6 +6,7 @@ const domCrawler = (node, accept, reject) =>
     domCrawler.map(node, n => n, accept, reject)
 ;
 
+
 /**
  * Create an object that can be used as a `NodeFilter`.
  *
@@ -25,6 +26,7 @@ domCrawler.createFilter = (
     }};
 };
 
+
 /**
  * Call the function on filtered nodes and return the results.
  * Removing the node in the function may interrupt the traversal.
@@ -42,6 +44,7 @@ domCrawler.map = (node = document, func, accept, reject) => {
     return result;
 };
 
+
 /**
  * Traverse all text nodes.
  * Removing the node in the function may interrupt the traversal.
@@ -56,6 +59,7 @@ domCrawler.mapTextNodes = (node, func, reject) =>
     domCrawler.map(node, func, n => n.nodeType == 3, reject)
 ;
 
+
 /**
  * Get all text nodes within the specified node.
  * @return Array of text nodes in `node` but not within those fit `reject`
@@ -63,6 +67,7 @@ domCrawler.mapTextNodes = (node, func, reject) =>
 domCrawler.getTextNodes = (node, reject) =>
     domCrawler.mapTextNodes(node, n => n, reject)
 ;
+
 
 /**
  * Replace substrings into whatever, and glue them into an array.
@@ -100,6 +105,30 @@ domCrawler.strSplitAndJoin = (str, separator, replacer) => {
     return result;
 };
 
+
+/**
+ * Replace text by the rules specified.
+ * It just applies multi rules to `domCrawler.strSplitAndJoin`.
+ * @param {Object[]} rules
+ * @param {string|RegExp} rules[].pattern
+ * @param {*} rules[].replacer
+ * @param {number} rules[].minLength
+ */
+domCrawler.strSplitAndJoinByRules = (str, rules) => {
+    if(!rules.forEach) rules = [rules];
+    return rules.reduce((splitted, rule) => {
+        for(let i = splitted.length - 1; i >= 0; --i) {
+            const frag = splitted[i];
+            if(typeof frag != "string" || (rule.minLength && frag.length < rule.minLength)) continue;
+            const debris = domCrawler.strSplitAndJoin(frag, rule.pattern, rule.replacer);
+            if(debris.length == 1 && debris[0] == frag) continue;
+            splitted.splice(i, 1, ...debris);
+        }
+        return splitted;
+    }, [str]);
+};
+
+
 /**
  * Replaces texts in the node by the rules specified.
  * To minimize DOM manipulation, let's iterate rules within the iteration of text nodes.
@@ -108,29 +137,23 @@ domCrawler.strSplitAndJoin = (str, separator, replacer) => {
  * @param {string|RegExp} rules[].pattern
  * @param {*} rules[].replacer
  * @param {number} rules[].minLength
+ * @param {Function} wrapper - This is called on the whole array `strSplitAndJoinByRules` returned. Afterwards the returned array this function returns will replace the origin content of the text node.
  */
 domCrawler.replaceTexts = (
     rules,
     node = document,
-    reject = n => ["SCRIPT", "STYLE"].includes(n.tagName)
+    reject = n => ["SCRIPT", "STYLE"].includes(n.tagName),
+    wrapper = null
 ) => {
-    if(!rules.forEach) rules = [rules];
     domCrawler.getTextNodes(node, reject).forEach(textNode => {
-        let splitted = rules.reduce((splitted, rule) => {
-            for(let i = splitted.length - 1; i >= 0; --i) {
-                const frag = splitted[i];
-                if(typeof frag != "string" || (rule.minLength && frag.length < rule.minLength)) continue;
-                const debris = domCrawler.strSplitAndJoin(frag, rule.pattern, rule.replacer);
-                if(debris.length == 1 && debris[0] == frag) continue;
-                splitted.splice(i, 1, ...debris);
-            }
-            return splitted;
-        }, [textNode.textContent]);
+        let splitted = domCrawler.strSplitAndJoinByRules(textNode.textContent, rules);
+        if(typeof wrapper == "function") splitted = wrapper.call(textNode, splitted);
         if(splitted.length == 1 && splitted[0] == textNode.textContent) return;
         textNode.replaceWith(...splitted);
     });
     node.normalize();
 };
+
 
 /**
  * Simulate React.createElement
@@ -143,12 +166,21 @@ domCrawler.createElement = (type, props, ...children) => {
             case "className":
                 elem.className = props.className;
                 break;
+            case "data":
+            case "dataset":
+                /**
+                 * It's also OK to do:
+                 * Element.setAttribute("data-something", "string value")
+                 * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset#Syntax }
+                 */
+                for(let ds in props[attr]) elem.dataset[ds] = props[attr][ds];
+                break;
             case "style":
                 /**
                  * React implements inline styles with an object whose key is the camelCased version of the style name,
                  * so here we cannot use CSSStyleDeclaration.setProperty().
-                 * @see {@link https://reactjs.org/docs/dom-elements.html#style|React}
-                 * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/setProperty|MDN}
+                 * @see {@link https://reactjs.org/docs/dom-elements.html#style }
+                 * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/setProperty }
                  */
                 if(typeof props.style == "string") elem.style.cssText = props.style;
                 else for(let sp in props.style) elem.style[sp] = props.style[sp];
@@ -160,5 +192,6 @@ domCrawler.createElement = (type, props, ...children) => {
     elem.append(...children);
     return elem;
 };
+
 
 if(typeof module === 'object') module.exports = domCrawler;
